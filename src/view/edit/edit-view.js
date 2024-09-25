6270abdfe4bfb025dda6b12caad5f24b74b1866f
mbeliar <93191478+igninus@users.noreply.github.com>
2024-09-25 17:10:11 +0400

    feat: inherit AbstractStatefulView

--- src/view/edit/edit-view.js
+++ src/view/edit/edit-view.js
@@ -1,6 +1,6 @@
-import AbstractView from '/src/framework/view/abstract-view';
 import createEventHeaderTemplate from './template/event-header';
 import createEventDetailsTemplate from './template/event-details';
+import AbstractStatefulView from '/src/framework/view/abstract-stateful-view';
 
 
 const createEditTemplate = (waypoint, allTypeOffers, destinations) =>
@@ -10,18 +10,21 @@ const createEditTemplate = (waypoint, allTypeOffers, destinations) =>
   </form>`;
 
 
-export default class EditView extends AbstractView {
-  #waypoint = null;
+export default class EditView extends AbstractStatefulView {
   #allTypeOffers = null;
   #handleEditClick = null;
+  #handleFormSubmit = null;
   #destinations = [];
+  #waypoint = null;
 
-  constructor({waypoint, allTypeOffers, destinations, onEditClick}) {
+  constructor({waypoint, allTypeOffers, destinations, onEditClick, onFormSubmit}) {
     super();
+    this._setState(EditView.parseWaypointToState(waypoint));
     this.#waypoint = waypoint;
     this.#destinations = destinations;
     this.#allTypeOffers = allTypeOffers;
     this.#handleEditClick = onEditClick;
+    this.#handleFormSubmit = onFormSubmit;
 
     this.element.querySelector('.event__rollup-btn')
       .addEventListener('click', this.#editClickHandler);
@@ -30,7 +33,8 @@ export default class EditView extends AbstractView {
   }
 
   get template() {
-    return createEditTemplate(this.#waypoint, this.#allTypeOffers, this.#destinations);
+    // return createEditTemplate(this.#waypoint, this.#allTypeOffers, this.#destinations);
+    return createEditTemplate(this._state, this.#allTypeOffers, this.#destinations);
   }
 
   #editClickHandler = (evt) => {
@@ -40,5 +44,18 @@ export default class EditView extends AbstractView {
 
   #formSubmitHandler = (evt) => {
     evt.preventDefault();
+
+    const waypoint = EditView.parseStateToWaypoint(this._state);
+    this.#handleFormSubmit(waypoint);
   };
+
+
+  static parseWaypointToState(waypoint) {
+    // return {...waypoint};
+    return waypoint;
+  }
+
+  static parseStateToWaypoint(state) {
+    return {...state};
+  }
 }
